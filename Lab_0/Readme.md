6b95df011492dd03b066fbe54394383b42c3b13e
Olkhovi4_Laptop <OlkhovskiyA2008@yandex.ru>
2025-09-13 12:19:37 +0300

    Added project Lab_0; base of Lab_1; gitignore

---
+++ Lab_0/Readme.md
@@ -0,0 +1,1 @@
+This is Lab number 0, where we were asked to write a program, which finds roots of square equasion.
---
+++ Lab_0/Testing.c
@@ -0,0 +1,118 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+
+void clear_screen() {
+#ifdef _WIN32
+    system("cls");
+#else
+    system("clear");
+#endif
+}
+
+
+double sqrt_x(double x){
+    double cur = x / 2;
+    double prev;
+    do{
+        prev = cur;
+        cur = 0.5 * (prev + x / prev);
+    }while((cur - prev) > 1e-9 || (prev - cur) > 1e-9);
+    return cur;
+}
+
+/* 
+2) придумать еще прикольных фичей
+4) Потестить на баги
+*/
+int main(){
+    setlocale(LC_ALL, ".UTF8");
+    double a, b, c;
+    char ans;
+    do{
+        clear_screen();
+        printf("\033[2J\033[H");
+        fflush(stdout);
+        printf("Введите коэффициенты a, b, c:\n");
+        scanf("%lf %lf %lf", &a, &b, &c);
+        printf("\nВведенное уравнение: %.3g * X^2 + %.3g * X + %.3g = 0\n\n", a, b, c);
+        printf("Подтвердите корректность вписанного уравнения (y/n): ");
+        scanf(" %c", &ans);
+    }while(ans != 'y');
+    printf("\n");
+
+    /*
+    int accuracy;
+    printf("Введите точность округления(кол-во знаков после запятой в ответе):\n");
+    scanf("%d", accuracy);
+    */
+    double dis = b * b - 4 * a * c;
+    if(a == 0.0){
+        if(b == 0.0 & c == 0.0){
+                printf("Уравнение имеет бесконечное кол-во решений\n\n");
+        }
+        else if(b == 0){
+                printf("Уравнение не имеет решений\n\n");
+        }
+        else if(c == 0){
+            printf("Линейное уравнение\n\n");
+            double res = -1 * c / b;
+            printf("X = 0\n");           
+        }
+        else{
+            printf("Линейное уравнение\n\n");
+            double res = -1 * c / b;
+            printf("X = %.3g\n", res);
+        }
+    }
+    else{
+        if(dis < 0){
+            printf("Дискриминант меньше 0\n\n");
+            dis *= -1;
+            double res_re, res_im;
+            res_re = -1 * b / (2 * a);
+            res_im = sqrt_x(dis) / (2 * a); 
+            printf("X1 = %.3g + i*%.3g\n", res_re, res_im);
+            printf("X2 = %.3g - i*%.3g\n", res_re, res_im);
+
+        }
+        else if(dis == 0){
+            printf("Дискриминант равен 0\n\n");
+            double res;
+            res = -1 * b / (2 * a);
+            printf("X1 = X2 = %.3g\n", res);
+
+        }
+        else{
+            printf("Дискриминант больше 0\n\n");
+            double x1, x2, sqrt_dis;
+            sqrt_dis = sqrt_x(dis);
+            x1 = (-1 * b + sqrt_dis) / (2 * a);
+            x2 = (-1 * b - sqrt_dis) / (2 * a);
+            printf("X1 = %.3g\n", x1);
+            printf("X2 = %.3g\n", x2);
+        }
+
+        printf("\nКоординаты вершины:\n");
+        double xPoint = -1 * b / 2 * a, yPoint = a * (xPoint * xPoint) + b * xPoint + c;
+        printf("X вершины = %.3g, Y вершины = %.3g\n", xPoint, yPoint);
+
+        printf("\nПроизводная:\n");
+        double der1 = 2 * a, der2 = b;
+        if(der2 < 0){
+            printf("Y = %.3g * x - %.3g\n\n", der1, -1 * der2);
+        }
+        else{
+            printf("Y = %.3g * x + %.3g\n\n", der1, der2);
+        }
+        
+    }
+    system("pause");
+    return 0;
+}
+
+
+/* printf("%.0f %.0f %.2f", a * 5, b * 3, c / 2); */
+/* gcc Lab_0.c &  .\a.exe */
+/*gcc Lab_0.c &  .\a.exe*/
