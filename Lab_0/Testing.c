15a59c00f0996ea886adb460e4851d21e024adaf
Olkhovi4_Laptop <OlkhovskiyA2008@yandex.ru>
2025-09-17 17:48:35 +0300

    Reworked mistakes. End of Lab_0.(i hope)

--- Lab_0/Testing.c
+++ Lab_0/Testing.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <locale.h>
 #include <windows.h>
 
 
@@ -26,41 +25,55 @@ double sqrt_x(double x){
 
 void init(double *a, double *b, double *c){
     char ans;
+    // SetConsoleOutputCP
     do{
         clear_screen();
         fflush(stdout);
-        printf("Р’РІРµРґРёС‚Рµ РєРѕСЌС„С„РёС†РёРµРЅС‚С‹ a, b, c:\n");
+        printf("Введите коэффициенты a, b, c:\n");
         scanf("%lf %lf %lf", a, b, c);
-        printf("\nР’РІРµРґРµРЅРЅРѕРµ СѓСЂР°РІРЅРµРЅРёРµ: %.3g * X^2 + %.3g * X + %.3g = 0\n\n", *a, *b, *c);
-        printf("РџРѕРґС‚РІРµСЂРґРёС‚Рµ РєРѕСЂСЂРµРєС‚РЅРѕСЃС‚СЊ РІРїРёСЃР°РЅРЅРѕРіРѕ СѓСЂР°РІРЅРµРЅРёСЏ (y/n): ");
+        printf("\nВведенное уравнение: %.3g * X^2 + %.3g * X + %.3g = 0\n\n", *a, *b, *c);
+        printf("Подтвердите корректность вписанного уравнения (y/n): ");
         scanf(" %c", &ans);
     }while(ans != 'y');
 }
 
 
-void output(double a, double  b, double c){
+int main(){
+
+    double a = 0.0, b = 0.0, c = 0.0;
+
+    init(&a, &b, &c);
+
+    printf("\n");
+    
+    /*
+    int accuracy;
+    printf("Введите точность округления(кол-во знаков после запятой в ответе):\n");
+    scanf("%d", accuracy);
+    */
+    
     double dis = b * b - 4 * a * c;
     if(a == 0.0){
         if(b == 0.0 & c == 0.0){
-                printf("РЈСЂР°РІРЅРµРЅРёРµ РёРјРµРµС‚ Р±РµСЃРєРѕРЅРµС‡РЅРѕРµ РєРѕР»-РІРѕ СЂРµС€РµРЅРёР№\n\n");
+                printf("Уравнение имеет бесконечное кол-во решений\n\n");
         }
         else if(b == 0){
-                printf("РЈСЂР°РІРЅРµРЅРёРµ РЅРµ РёРјРµРµС‚ СЂРµС€РµРЅРёР№\n\n");
+                printf("Уравнение не имеет решений\n\n");
         }
         else if(c == 0){
-            printf("Р›РёРЅРµР№РЅРѕРµ СѓСЂР°РІРЅРµРЅРёРµ\n\n");
+            printf("Линейное уравнение\n\n");
             double res = -1 * c / b;
             printf("X = 0\n");           
         }
         else{
-            printf("Р›РёРЅРµР№РЅРѕРµ СѓСЂР°РІРЅРµРЅРёРµ\n\n");
+            printf("Линейное уравнение\n\n");
             double res = -1 * c / b;
             printf("X = %.3g\n", res);
         }
     }
     else{
         if(dis < 0){
-            printf("Р”РёСЃРєСЂРёРјРёРЅР°РЅС‚ РјРµРЅСЊС€Рµ 0\n\n");
+            printf("Дискриминант меньше 0\n\n");
             dis *= -1;
             double res_re = 0.0, res_im = 0.0;
             res_re = -1 * b / (2 * a);
@@ -70,14 +83,14 @@ void output(double a, double  b, double c){
 
         }
         else if(dis == 0){
-            printf("Р”РёСЃРєСЂРёРјРёРЅР°РЅС‚ СЂР°РІРµРЅ 0\n\n");
+            printf("Дискриминант равен 0\n\n");
             double res = 0.0;
             res = -1 * b / (2 * a);
             printf("X1 = X2 = %.3g\n", res);
 
         }
         else{
-            printf("Р”РёСЃРєСЂРёРјРёРЅР°РЅС‚ Р±РѕР»СЊС€Рµ 0\n\n");
+            printf("Дискриминант больше 0\n\n");
             double x1 = 0.0, x2 = 0.0, sqrt_dis = 0.0;
             sqrt_dis = sqrt_x(dis);
             x1 = (-1 * b + sqrt_dis) / (2 * a);
@@ -86,11 +99,11 @@ void output(double a, double  b, double c){
             printf("X2 = %.3g\n", x2);
         }
 
-        printf("\nРљРѕРѕСЂРґРёРЅР°С‚С‹ РІРµСЂС€РёРЅС‹:\n");
+        printf("\nКоординаты вершины:\n");
         double xPoint = -1 * b / 2 * a, yPoint = a * (xPoint * xPoint) + b * xPoint + c;
-        printf("X РІРµСЂС€РёРЅС‹ = %.3g, Y РІРµСЂС€РёРЅС‹ = %.3g\n", xPoint, yPoint);
+        printf("X вершины = %.3g, Y вершины = %.3g\n", xPoint, yPoint);
         
-        printf("\nРџСЂРѕРёР·РІРѕРґРЅР°СЏ:\n");
+        printf("\nПроизводная:\n");
         double der1 = 2 * a, der2 = b;
         if(der2 < 0){
             printf("Y = %.3g * x - %.3g\n", der1, -1 * der2);
@@ -101,40 +114,20 @@ void output(double a, double  b, double c){
         double zero_der = 0.0;
         zero_der = -1 * der2 / der1;
         if(a < 0){
-            printf("РџСЂРё x Оµ (-в€ћ; %.3g) f`(x) > 0 => С„СѓРЅРєС†РёСЏ РІРѕР·СЂР°СЃС‚Р°РµС‚\n", zero_der);
-            printf("РџСЂРё x Оµ (%.3g; в€ћ)  f`(x) < 0 => С„СѓРЅРєС†РёСЏ СѓР±С‹РІР°РµС‚\n\n", zero_der);
+            printf("При x ? (-?; %.3g) f`(x) > 0 => функция возрастает\n", zero_der);
+            printf("При x ? (%.3g; ?)  f`(x) < 0 => функция убывает\n\n", zero_der);
         }
         else{
-            printf("РџСЂРё x Оµ (-в€ћ; %.3g) f`(x) < 0 => С„СѓРЅРєС†РёСЏ СѓР±С‹РІР°РµС‚\n", zero_der);
-            printf("РџСЂРё x Оµ (%.3g; в€ћ)  f`(x) > 0 => С„СѓРЅРєС†РёСЏ РІРѕР·СЂР°СЃС‚Р°РµС‚\n\n", zero_der);
+            printf("При x ? (-?; %.3g) f`(x) < 0 => функция убывает\n", zero_der);
+            printf("При x ? (%.3g; ?)  f`(x) > 0 => функция возрастает\n\n", zero_der);
         }
         
     }
-}
-
-
-int main(){
-    setlocale(LC_ALL, ".UTF8");
-    SetConsoleOutputCP(CP_UTF8);
-
-    double a = 0.0, b = 0.0, c = 0.0;
-
-    init(&a, &b, &c);
-
-    printf("\n");
-    
-    /*
-    int accuracy;
-    printf("Р’РІРµРґРёС‚Рµ С‚РѕС‡РЅРѕСЃС‚СЊ РѕРєСЂСѓРіР»РµРЅРёСЏ(РєРѕР»-РІРѕ Р·РЅР°РєРѕРІ РїРѕСЃР»Рµ Р·Р°РїСЏС‚РѕР№ РІ РѕС‚РІРµС‚Рµ):\n");
-    scanf("%d", accuracy);
-    */
-    
-    output(a, b, c);
 
     system("pause");
     return 0;
 }
-
+// chcp 1251
 
 /* printf("%.0f %.0f %.2f", a * 5, b * 3, c / 2); */
 /* gcc Lab_0.c &  .\a.exe */
